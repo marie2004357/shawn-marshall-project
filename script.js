443b9a34f46c07803e11cb66b8ef5b6ec48732ac
shawnmarshall <33719153+shawnmarshall@users.noreply.github.com>
2017-11-16 22:34:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// This is the message box displayed when the Join button is clicked
 function myFunction() {
     alert("I'm sorry this feature is under construction.  Please contact me via email");
 }
